6fea13339a297ad3349ec3e1cc54301566db1153
Aoi Sogen <aoooi522@outlook.jp>
2026-06-05 02:52:52 +0900

    portfolio#2

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,27 @@ window.addEventListener("load", () => {
     });
   }
 
+  /**
+  * オプション選択後の枠色変更の処理
+  */
+  function toggleOption(){
+    const optionBoxes = document.querySelectorAll(".p-optionBox");
+
+    optionBoxes.forEach(function(box) {
+      const checkbox = box.querySelector(".p-optionBox__input");
+
+      checkbox.addEventListener("change", function() {
+        if(checkbox.checked) {
+          box.classList.add("is-selected");
+        } else {
+          box.classList.remove("is-selected");
+        }
+      });
+    });
+  }
+
   changeButtonText();
   selectCard(".p-planBox");
   selectCard(".p-frequencyBox");
+  toggleOption();
 });
